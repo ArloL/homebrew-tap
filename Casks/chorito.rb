d510644faf95e9b25ec2c4dc71dca111dff585d6
Arlo O'Keeffe <tiger@k5d.de>
2022-11-29 17:19:23 +0100

    Update chorito from 0.0.233 to 0.0.234

--- Casks/chorito.rb
+++ Casks/chorito.rb
@@ -1,6 +1,6 @@
 cask "chorito" do
-  version "0.0.233"
-  sha256 "2a8f53e4213eaee6aa0ba7034f111f0a68bf5fd99595202f160392e4884b3bce"
+  version "0.0.234"
+  sha256 "e5d635b8d3f80d910fa65392703b02c4429a16ec6c3a55e2f340f5585ea090b8"
 
   url "https://github.com/ArloL/chorito/releases/download/v#{version}/chorito-macos-#{version}.zip"
   name "chorito"
