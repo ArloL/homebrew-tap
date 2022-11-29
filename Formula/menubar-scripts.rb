a62bb218bfbbe2c146938232f7da8e22f831d860
Arlo O'Keeffe <tiger@k5d.de>
2022-11-29 20:16:30 +0100

    update menubar-scripts

--- Formula/menubar-scripts.rb
+++ Formula/menubar-scripts.rb
@@ -1,13 +1,13 @@
 class MenubarScripts < Formula
   desc "Something something darkside"
   homepage "https://github.com/ArloL/menubar-scripts"
-  url "https://github.com/ArloL/menubar-scripts/archive/refs/tags/v0.0.3.tar.gz"
-  sha256 "144cf77a0e8ad8ebf331d6f88ec7a6e03b80b2e776b702e434d4e2c50302b850"
+  url "https://github.com/ArloL/menubar-scripts/archive/refs/tags/v0.0.4.tar.gz"
+  sha256 "6939e047f86165d652a0a23e45223fe7f6a9d3d0041afa01829f042a9187ecc2"
   license "MIT"
   head "https://github.com/ArloL/menubar-scripts.git", branch: "main"
 
   def install
-    bin.install "daemons.sh" => "menubar-scripts"
+    bin.install "service.sh" => "menubar-scripts"
   end
 
   service do
