bae5ae6add942653d386c576e622ed4f542657c1
Arlo O'Keeffe <tiger@k5d.de>
2022-11-29 17:19:48 +0100

    Update wait-for-ports from 0.0.191 to 0.0.192

--- Casks/wait-for-ports.rb
+++ Casks/wait-for-ports.rb
@@ -1,6 +1,6 @@
 cask "wait-for-ports" do
-  version "0.0.191"
-  sha256 "7601af2becd442fcab09066b87ab12091e1aeda476f3bb45b421f24b6e355bcf"
+  version "0.0.192"
+  sha256 "4b519bc9d2074665a3f5a85ab705dc263bbf162504cb072221ea28cbdc3ac919"
 
   url "https://github.com/ArloL/wait-for-ports/releases/download/v#{version}/wait-for-ports-macos-#{version}.zip"
   name "wait-for-ports"
