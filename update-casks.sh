607c4fca7c1acf58d434669f966f8566bb01331e
Arlo O'Keeffe <tiger@k5d.de>
2022-12-01 16:45:49 +0100

    add brew tap

--- update-casks.sh
+++ update-casks.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
+brew tap arlol/tap
 brew bump --cask --open-pr arlol/tap/chorito
 brew bump --cask --open-pr arlol/tap/newlinechecker
 brew bump --cask --open-pr arlol/tap/wait-for-ports
