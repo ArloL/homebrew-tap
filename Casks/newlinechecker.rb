79c25bc9baeaf5a001da681ebe326f1e1d1ba342
Arlo O'Keeffe <tiger@k5d.de>
2022-12-01 16:43:41 +0100

    Update newlinechecker from 0.0.241 to 0.0.243

--- Casks/newlinechecker.rb
+++ Casks/newlinechecker.rb
@@ -1,6 +1,6 @@
 cask "newlinechecker" do
-  version "0.0.241"
-  sha256 "f03217765198939c99057575f0b0beec087da7020a3bee08bdac500e287444ae"
+  version "0.0.243"
+  sha256 "404e41c457c15198ac91cbb585174cc109b30c0778c650aea530beb33db42264"
 
   url "https://github.com/ArloL/newlinechecker/releases/download/v#{version}/newlinechecker-macos-#{version}.zip"
   name "newlinechecker"
