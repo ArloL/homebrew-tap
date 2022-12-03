3c8346a99d80ffd5d08115770eeb4fea9a74d951
ArloL <ArloL@users.noreply.github.com>
2022-12-03 14:30:28 +0000

    update tap

--- Casks/wait-for-ports.rb
+++ Casks/wait-for-ports.rb
@@ -1,6 +1,6 @@
 cask "wait-for-ports" do
-  version "0.0.194"
-  sha256 "20c5c15c1af1685eb5a2f3e7f138820e25d6e0d07e78ccdaefe17991acb38e89"
+  version "0.0.196"
+  sha256 "f8a4d3f0b08bd05bc8c09576eca26cdec57a2a141f80cad0256c9d5050ec0a22"
 
   url "https://github.com/ArloL/wait-for-ports/releases/download/v#{version}/wait-for-ports-macos-#{version}.zip"
   name "wait-for-ports"
