01e9d0a545a5c9481f052cf6b937199e9a641abb
ArloL <ArloL@users.noreply.github.com>
2022-12-03 14:50:06 +0000

    update tap

--- Formula/rss-to-mail.rb
+++ Formula/rss-to-mail.rb
@@ -1,8 +1,8 @@
 class RssToMail < Formula
   desc "Something something darkside"
   homepage "https://github.com/ArloL/rss-to-mail"
-  url "https://github.com/ArloL/rss-to-mail/archive/refs/tags/v0.0.16.tar.gz"
-  sha256 "2011ce252fe93958bf54816ffca146ebbfca86e24fe5fda392184c180946eeb2"
+  url "https://github.com/ArloL/rss-to-mail/archive/refs/tags/v0.0.17.tar.gz"
+  sha256 "7c81c10d92e697fc7fcb507e21af5324518c426fc14cc96a4fd910a4baa38819"
   license "MIT"
   head "https://github.com/ArloL/rss-to-mail.git", branch: "main"
 
