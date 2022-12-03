25a4836c020f671899b320ae1d909dab8468023c
Arlo O'Keeffe <tiger@k5d.de>
2022-12-03 14:30:28 +0100

    use macos sed

--- update-tap.sh
+++ update-tap.sh
@@ -4,18 +4,16 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-hash gsed 2>/dev/null && SED="gsed" || SED="sed"
-
 check() {
     JSON=$(brew livecheck --quiet --json --"${1}" "${2}" \
         | jq --raw-output '.[]')
     VERSION_CURRENT=$(echo "$JSON" | jq --raw-output '.version.current')
     VERSION_LATEST=$(echo "$JSON" | jq --raw-output '.version.latest')
-    "${SED}" -i "s/${VERSION_CURRENT}/${VERSION_LATEST}/g" "${2}"
+    sed -i "" "s/${VERSION_CURRENT}/${VERSION_LATEST}/g" "${2}"
     SHA256_CURRENT=$(brew info --json=v2 --"${1}" "${2}" \
         | jq --raw-output "${3}")
     SHA256_LATEST=$(brew fetch --"${1}" "${2}" | grep "^SHA256:")
-    "${SED}" -i "s/${SHA256_CURRENT}/${SHA256_LATEST#SHA256: }/g" "${2}"
+    sed -i "" "s/${SHA256_CURRENT}/${SHA256_LATEST#SHA256: }/g" "${2}"
     brew audit --strict --online --"${1}" "${2}"
 }
 
