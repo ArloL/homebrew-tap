fea22cefefcec8d266fca15892d2ec142992696d
Arlo O'Keeffe <tiger@k5d.de>
2022-12-04 13:23:21 +0100

    add service to rss-to-mail

--- Formula/rss-to-mail.rb
+++ Formula/rss-to-mail.rb
@@ -13,6 +13,16 @@ class RssToMail < Formula
     system "./mvnw", "--batch-mode", "clean", "package", "-DskipTests", "-Drevision=#{version}"
     libexec.install "target/rss-to-mail-#{version}.jar"
     bin.write_jar_script libexec/"rss-to-mail-#{version}.jar", "rss-to-mail"
+    (var/"rss-to-mail").mkpath
+  end
+
+  service do
+    run opt_bin/"rss-to-mail"
+    run_type :interval
+    interval 1500 # seconds; 25 minutes
+    log_path var/"log/rss-to-mail.log"
+    error_log_path var/"log/rss-to-mail.log"
+    working_dir var/"rss-to-mail"
   end
 
   test do
