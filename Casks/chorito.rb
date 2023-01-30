36584ee5f7f6fb582c73f7566977dd0ca4d409e4
ArloL <ArloL@users.noreply.github.com>
2023-01-30 09:19:39 +0000

    update tap

--- Casks/chorito.rb
+++ Casks/chorito.rb
@@ -1,6 +1,6 @@
 cask "chorito" do
-  version "0.0.267"
-  sha256 "270fa28b9f619169b65bd5abb29629bf422dff7343afaf2c95a24238f19e98d6"
+  version "0.0.268"
+  sha256 "a1c078e807fa771ecf159b2b33848b052ee64aa47305d4dbf1847dc1b79b821e"
 
   url "https://github.com/ArloL/chorito/releases/download/v#{version}/chorito-macos-#{version}.zip"
   name "chorito"
--- Casks/wait-for-ports.rb
+++ Casks/wait-for-ports.rb
@@ -1,6 +1,6 @@
 cask "wait-for-ports" do
-  version "0.0.217"
-  sha256 "a57f069750b3732b546bf41c42e5e859581d9d059eee5ac3329f84823b1c64fd"
+  version "0.0.218"
+  sha256 "d358ae25b3551a3c11ed8a29d364f2284e62cfa0def56561be5c2c3c7af18e4e"
 
   url "https://github.com/ArloL/wait-for-ports/releases/download/v#{version}/wait-for-ports-macos-#{version}.zip"
   name "wait-for-ports"
