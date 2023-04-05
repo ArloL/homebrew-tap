ed3a42ba384b42761021e784c07291c68fbe0387
ArloL <ArloL@users.noreply.github.com>
2023-04-05 11:59:06 +0000

    update tap

--- Casks/newlinechecker.rb
+++ Casks/newlinechecker.rb
@@ -1,6 +1,6 @@
 cask "newlinechecker" do
-  version "0.0.294"
-  sha256 "d62c6bc2f5bfbd31571434c650bf60e7d15c17797f08b50659379251a8cd69bb"
+  version "0.0.295"
+  sha256 "7279634ffa7f13f458b0196ac99fc7325b03f3af69a4d0c02719f3718c85eeda"
 
   url "https://github.com/ArloL/newlinechecker/releases/download/v#{version}/newlinechecker-macos-#{version}.zip"
   name "newlinechecker"
