2b59f5836bda605fd80aecaadae26c41f8ef4fda
ArloL <ArloL@users.noreply.github.com>
2023-05-02 08:07:39 +0000

    update tap

--- Formula/menubar-scripts.rb
+++ Formula/menubar-scripts.rb
@@ -1,8 +1,8 @@
 class MenubarScripts < Formula
   desc "Something something darkside"
   homepage "https://github.com/ArloL/menubar-scripts"
-  url "https://github.com/ArloL/menubar-scripts/archive/refs/tags/v0.0.21.tar.gz"
-  sha256 "ae3600630743e42e518db55d2934e5d8abb97979e070bd0b144bf87226c8ddf5"
+  url "https://github.com/ArloL/menubar-scripts/archive/refs/tags/v0.0.22.tar.gz"
+  sha256 "dd03fea13ae734077cd72167ced638dd14ad238bf21d764b9f8a94d3428a6f48"
   license "MIT"
   head "https://github.com/ArloL/menubar-scripts.git", branch: "main"
 
