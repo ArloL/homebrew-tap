d2a60e26010d320a43ef41a05dd66869ec1e6197
ArloL <ArloL@users.noreply.github.com>
2023-09-13 06:20:23 +0000

    update tap

--- Casks/newlinechecker.rb
+++ Casks/newlinechecker.rb
@@ -1,6 +1,6 @@
 cask "newlinechecker" do
-  version "0.0.347"
-  sha256 "35ac9a462e694c1a266a8f5d410d2a09146218e7ff10022e838da772b7369b79"
+  version "0.0.348"
+  sha256 "93ad3b5af85731b5dfbd00e8ea09dc839554f2f75be10bb103147b2ba00fe2f9"
 
   url "https://github.com/ArloL/newlinechecker/releases/download/v#{version}/newlinechecker-macos-#{version}.zip"
   name "newlinechecker"
--- Casks/wait-for-ports.rb
+++ Casks/wait-for-ports.rb
@@ -1,6 +1,6 @@
 cask "wait-for-ports" do
-  version "0.0.290"
-  sha256 "2b7fd5b31b56f6b50e82624d174b746ae9dfcb6e08fac765e6066890add2e247"
+  version "0.0.291"
+  sha256 "f125873f167976241e0464be6a4a07f9b3259a37e4448f177a84b550e822b42f"
 
   url "https://github.com/ArloL/wait-for-ports/releases/download/v#{version}/wait-for-ports-macos-#{version}.zip"
   name "wait-for-ports"
