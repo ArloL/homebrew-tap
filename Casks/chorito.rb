471092480426a8f200950aae69b8da4bb47c83fe
ArloL <ArloL@users.noreply.github.com>
2023-11-15 09:37:13 +0000

    update tap

--- Casks/chorito.rb
+++ Casks/chorito.rb
@@ -1,6 +1,6 @@
 cask "chorito" do
-  version "0.0.390"
-  sha256 "9d2e54807624283864fd7428087ae558b43c65f36f556accd93798b30c46daf5"
+  version "0.0.391"
+  sha256 "20b525ac834ff681046d1ee00ff4ed8791c095f27589b2a4f695478905322d7b"
 
   url "https://github.com/ArloL/chorito/releases/download/v#{version}/chorito-macos-#{version}.zip"
   name "chorito"
