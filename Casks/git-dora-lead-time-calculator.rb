9f1427ba6f98a23ae16ef95bb8252d7067cce3df
Arlo O'Keeffe <tiger@k5d.de>
2023-12-23 17:55:35 +0100

    add git-dora-lead-time-calculator

---
+++ Casks/git-dora-lead-time-calculator.rb
@@ -0,0 +1,11 @@
+cask "git-dora-lead-time-calculator" do
+  version "0.0.2"
+  sha256 "d4d38cd0cb817853262bc778d66575d640699fd2362ae314adb1e1b399fc25c9"
+
+  url "https://github.com/ArloL/git-dora-lead-time-calculator/releases/download/v#{version}/git-dora-lead-time-calculator-macos-#{version}.zip"
+  name "git-dora-lead-time-calculator"
+  desc "Something something darkside"
+  homepage "https://github.com/ArloL/git-dora-lead-time-calculator"
+
+  binary "#{staged_path}/git-dora-lead-time-calculator-macos-#{version}/git-dora-lead-time-calculator-macos-#{version}", target: "git-dora-lead-time-calculator"
+end
