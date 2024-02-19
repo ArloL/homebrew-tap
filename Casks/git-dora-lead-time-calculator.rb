1b2f034d6abd39705b1330142d2d1fc40e73160d
ArloL <195367+ArloL@users.noreply.github.com>
2024-02-19 07:56:09 +0000

    update tap

--- Casks/git-dora-lead-time-calculator.rb
+++ Casks/git-dora-lead-time-calculator.rb
@@ -1,6 +1,6 @@
 cask "git-dora-lead-time-calculator" do
-  version "2402.0.111"
-  sha256 "84ff6689fada690da0e5f24ef845464987ef04f1b69f417b8e5ed5a1e0495171"
+  version "2402.0.112"
+  sha256 "fc2a735254c4c4aa5c67d597af9f2f76db1791ef14e53d7dfad9d31f6c425c1e"
 
   url "https://github.com/ArloL/git-dora-lead-time-calculator/releases/download/v#{version}/git-dora-lead-time-calculator-macos-#{version}.zip"
   name "git-dora-lead-time-calculator"
--- Formula/menubar-scripts.rb
+++ Formula/menubar-scripts.rb
@@ -1,8 +1,8 @@
 class MenubarScripts < Formula
   desc "Something something darkside"
   homepage "https://github.com/ArloL/menubar-scripts"
-  url "https://github.com/ArloL/menubar-scripts/archive/refs/tags/v0.0.48.tar.gz"
-  sha256 "336991abe92d850a4bbaa804c0c9d204ccf217809249e3cf099ce185905785f6"
+  url "https://github.com/ArloL/menubar-scripts/archive/refs/tags/v0.0.49.tar.gz"
+  sha256 "b83579164592e7687c62c327d2d576a38d3cfd88eabc7a47790d77ffa3ed3553"
   license "MIT"
   head "https://github.com/ArloL/menubar-scripts.git", branch: "main"
 
