013a12676a9bba2dab072ddcc44e86ff4a2486b2
Arlo O'Keeffe <tiger@k5d.de>
2024-06-18 17:56:20 +0200

    put braces around variable reference

--- update-tap.sh
+++ update-tap.sh
@@ -8,8 +8,8 @@ set -o pipefail
 check() {
     JSON=$(brew livecheck --quiet --json --"${1}" "${2}" \
         | jq --raw-output '.[]')
-    VERSION_CURRENT=$(echo "$JSON" | jq --raw-output '.version.current')
-    VERSION_LATEST=$(echo "$JSON" | jq --raw-output '.version.latest')
+    VERSION_CURRENT=$(echo "${JSON}" | jq --raw-output '.version.current')
+    VERSION_LATEST=$(echo "${JSON}" | jq --raw-output '.version.latest')
     sed -i "" "s/${VERSION_CURRENT}/${VERSION_LATEST}/g" "${3}"
     SHA256_CURRENT=$(brew info --json=v2 --"${1}" "${2}" \
         | jq --raw-output "${4}")
