6af7be99c2122136b93870d9e9de7cb7b96dbae0
ArloL <195367+ArloL@users.noreply.github.com>
2024-06-19 14:55:06 +0000

    update tap

--- Casks/chorito.rb
+++ Casks/chorito.rb
@@ -1,6 +1,6 @@
 cask "chorito" do
-  version "0.0.535"
-  sha256 "2d563a309444bbb34eb6b0044e633e09d7255b1066f0af1f52c498d8579cd7c2"
+  version "0.0.537"
+  sha256 "62ae461d55f3123a059944f8b1482ff47c1684ab4756b0dbc099ecb13149e4cd"
 
   url "https://github.com/ArloL/chorito/releases/download/v#{version}/chorito-macos-#{version}.zip"
   name "chorito"
--- Casks/git-dora-lead-time-calculator.rb
+++ Casks/git-dora-lead-time-calculator.rb
@@ -1,6 +1,6 @@
 cask "git-dora-lead-time-calculator" do
-  version "2406.0.105"
-  sha256 "119f5122778ece4c67f89959752fc7a297b15e024be7c35a643db4f0c7116a7f"
+  version "2406.0.999"
+  sha256 "b6fe43f37b0763d50c1b27c82a329628bd7f7d80a0d617d7a1614506a9d28e7a"
 
   url "https://github.com/ArloL/git-dora-lead-time-calculator/releases/download/v#{version}/git-dora-lead-time-calculator-macos-#{version}.zip"
   name "git-dora-lead-time-calculator"
