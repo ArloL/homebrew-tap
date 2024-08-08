d042b201457cb98e80a8696a44b6d56dafe07567
ArloL <195367+ArloL@users.noreply.github.com>
2024-08-08 18:31:53 +0000

    update tap

--- Casks/git-dora-lead-time-calculator.rb
+++ Casks/git-dora-lead-time-calculator.rb
@@ -1,6 +1,6 @@
 cask "git-dora-lead-time-calculator" do
-  version "2408.0.101"
-  sha256 "bad9219cf3a804fd6525af6215fd71e7b2ef12c45c2b62c71a7a83abf6fea871"
+  version "2408.0.104"
+  sha256 "be5784600aed0d1ea71a6a8f16e405f8d6911f31fcdbe1d13ef2d499dd19c128"
 
   url "https://github.com/ArloL/git-dora-lead-time-calculator/releases/download/v#{version}/git-dora-lead-time-calculator-macos-#{version}.zip"
   name "git-dora-lead-time-calculator"
--- Casks/wait-for-ports.rb
+++ Casks/wait-for-ports.rb
@@ -1,6 +1,6 @@
 cask "wait-for-ports" do
-  version "0.0.428"
-  sha256 "303e254381b6a8367332d64c1e6f94008f9c671647cd1dab02002c1fa241e406"
+  version "0.0.430"
+  sha256 "5e60449e552cdf3ecb635b2f836a24b0af6f62741d4006b03ce0b91e1d8ed7f8"
 
   url "https://github.com/ArloL/wait-for-ports/releases/download/v#{version}/wait-for-ports-macos-#{version}.zip"
   name "wait-for-ports"
