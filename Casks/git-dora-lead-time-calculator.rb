63b1fb827a22f3da0bed34e9d060acf06781ed49
ArloL <195367+ArloL@users.noreply.github.com>
2024-12-12 16:07:40 +0000

    update tap

--- Casks/git-dora-lead-time-calculator.rb
+++ Casks/git-dora-lead-time-calculator.rb
@@ -1,6 +1,6 @@
 cask "git-dora-lead-time-calculator" do
-  version "2412.0.103"
-  sha256 "c2fbdadc36bde4d1dc960d3523770e3ef9c129fa7e6bacc4afba9d43ca5ced9a"
+  version "2412.0.104"
+  sha256 "d13684c0542f2fee56cd72b333f8a6806f6ce9a0ecba4c1a6eca15fe6961d45e"
 
   url "https://github.com/ArloL/git-dora-lead-time-calculator/releases/download/v#{version}/git-dora-lead-time-calculator-macos-#{version}.zip"
   name "git-dora-lead-time-calculator"
--- Casks/newlinechecker.rb
+++ Casks/newlinechecker.rb
@@ -1,6 +1,6 @@
 cask "newlinechecker" do
-  version "2412.0.104"
-  sha256 "b5c19822b2dd12999f0b1cc497726a355ca4d9aedffa7cbaa9cfd465269890d6"
+  version "2412.0.105"
+  sha256 "3fed3089fa812e25ea7be424097eb18b976bb66198eace6d09b60a01b1a51e32"
 
   url "https://github.com/ArloL/newlinechecker/releases/download/v#{version}/newlinechecker-macos-#{version}.zip"
   name "newlinechecker"
