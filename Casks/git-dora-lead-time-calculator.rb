ca15c46b4b30743f6a75c37272ac411c4af36273
ArloL <195367+ArloL@users.noreply.github.com>
2025-01-13 08:09:49 +0000

    update tap

--- Casks/git-dora-lead-time-calculator.rb
+++ Casks/git-dora-lead-time-calculator.rb
@@ -1,6 +1,6 @@
 cask "git-dora-lead-time-calculator" do
-  version "2501.0.108"
-  sha256 "da21d0d60be3bead3aabaf2f9bafa1852e7ec0752d64197a50cbb3246773ffc4"
+  version "2501.0.109"
+  sha256 "0097e1184b9a9df57e3f3433ab9727b7fd4a4f5b7ce0860417904e511f2f1529"
 
   url "https://github.com/ArloL/git-dora-lead-time-calculator/releases/download/v#{version}/git-dora-lead-time-calculator-macos-#{version}.zip"
   name "git-dora-lead-time-calculator"
