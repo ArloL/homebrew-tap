4cf41e6aa1120b611b179b5558fa3f0251f38bd1
ArloL <195367+ArloL@users.noreply.github.com>
2025-03-02 00:50:39 +0000

    update tap

--- Casks/chorito.rb
+++ Casks/chorito.rb
@@ -1,6 +1,6 @@
 cask "chorito" do
-  version "2503.0.104"
-  sha256 "41d78678352ab297e9420a67918d461f13542ccecfb79977de4adbcd90983194"
+  version "2503.0.105"
+  sha256 "a6471cd3717974ca2320a273bdcbde0e17a342a543a1edab18295484c4376211"
 
   url "https://github.com/ArloL/chorito/releases/download/v#{version}/chorito-macos-#{version}.zip"
   name "chorito"
--- Casks/newlinechecker.rb
+++ Casks/newlinechecker.rb
@@ -1,6 +1,6 @@
 cask "newlinechecker" do
-  version "2503.0.104"
-  sha256 "4a5682eab99284404e77809df70ec44deb69d1da74c135e3cb57102d9582191b"
+  version "2503.0.105"
+  sha256 "8122997a9547975cbc084ddc072447cb877b125a0dead6db69919ac0bc4bf687"
 
   url "https://github.com/ArloL/newlinechecker/releases/download/v#{version}/newlinechecker-macos-#{version}.zip"
   name "newlinechecker"
