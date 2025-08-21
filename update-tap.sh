2d41264817ffc704072b553a6b2e42df4a730ca8
Arlo O'Keeffe <tiger@k5d.de>
2025-08-21 12:44:13 +0200

    dbg

--- update-tap.sh
+++ update-tap.sh
@@ -38,7 +38,7 @@ check_formula() {
   check "formula" "${TAP_NAME}/tap/${1}" "./Formula/${1}.rb" ".formulae[0].urls.stable.checksum"
 }
 
-ls -lsa "${HOMEBREW_TAP_REPOSITORY}"
+set
 
 TAPS_DIR="$(brew --repository)/Library/Taps/"
 TAP_DIR=$(mktemp --directory --tmpdir="${TAPS_DIR}")
