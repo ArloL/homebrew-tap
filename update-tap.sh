75bea04c48de68d51cc7a8aa90a5b92c38fee194
Arlo O'Keeffe <tiger@k5d.de>
2025-08-22 20:52:30 +0200

    use full tap name instead of file

--- update-tap.sh
+++ update-tap.sh
@@ -5,6 +5,8 @@ set -o nounset
 set -o xtrace
 set -o pipefail
 
+export HOMEBREW_DEVELOPER=yes
+
 check() {
   JSON=$(brew livecheck --quiet --json --"${1}" "${2}" |
     jq --raw-output '.[]')
@@ -13,7 +15,7 @@ check() {
   sed -i "" "s/${VERSION_CURRENT}/${VERSION_LATEST}/g" "${3}"
   SHA256_CURRENT=$(brew info --json=v2 --"${1}" "${2}" |
     jq --raw-output "${4}")
-  SHA256_LATEST=$(brew fetch --"${1}" "${3}")
+  SHA256_LATEST=$(brew fetch --"${1}" "${2}")
   SHA256_LATEST=$(set +o pipefail && echo "${SHA256_LATEST}" | grep -m 1 "^SHA-256:")
   SHA256_LATEST=${SHA256_LATEST#SHA-256: }
   sed -i "" "s/${SHA256_CURRENT}/${SHA256_LATEST}/g" "${3}"
@@ -21,11 +23,11 @@ check() {
 }
 
 check_cask() {
-  check "cask" "${1}" "./Casks/${1}.rb" ".casks[0].sha256"
+  check "cask" "arlol/tap/${1}" "./Casks/${1}.rb" ".casks[0].sha256"
 }
 
 check_formula() {
-  check "formula" "${1}" "./Formula/${1}.rb" ".formulae[0].urls.stable.checksum"
+  check "formula" "arlol/tap/${1}" "./Formula/${1}.rb" ".formulae[0].urls.stable.checksum"
 }
 
 check_cask chorito
