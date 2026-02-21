6b14cd618a7df23e62ff1a9297d1a08e0f744c02
Arlo O'Keeffe <tiger@k5d.de>
2026-02-21 13:31:04 +0100

    detect java home a little smarter

--- Formula/wait-for-ports.rb
+++ Formula/wait-for-ports.rb
@@ -7,7 +7,12 @@ class WaitForPorts < Formula
   head "https://github.com/ArloL/wait-for-ports.git", branch: "main"
 
   def install
-    ENV["JAVA_HOME"] = "/Library/Java/JavaVirtualMachines/graalvm-25.jdk/Contents/Home"
+    java_home = ENV["JAVA_HOME"]
+    ENV["JAVA_HOME"] = if java_home && File.exist?("#{java_home}/lib/graal")
+      java_home
+    else
+      ENV.fetch("GRAALVM_HOME", "/Library/Java/JavaVirtualMachines/graalvm-25.jdk/Contents/Home")
+    end
     # homebrew adds shims to PATH that check for ruby when native-maven-plugin
     # calls cc. but it resets the environment sooo we remove them ¯\_(ツ)_/¯
     ENV["PATH"] = "/usr/bin:/bin:/usr/sbin:/sbin"
